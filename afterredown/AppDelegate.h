b5a23517ce017548e91e3eb8f8cc470fe3749332
xsy <xsy@xsydeMacBook-Pro.local>
2015-07-20 07:48:16 +0800

    Initial Commit

---
+++ afterredown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  afterredown
+//
+//  Created by xsy on 15/7/20.
+//  Copyright (c) 2015年 xsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ afterredown/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  afterredown
+//
+//  Created by xsy on 15/7/20.
+//  Copyright (c) 2015年 xsy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
